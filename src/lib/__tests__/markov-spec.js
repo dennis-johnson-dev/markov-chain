ee1a8249c631fedb11d019ff569d603cb614696f
songawee <songawee@gmail.com>
2016-12-27 18:36:30 -0700

    add source API

--- src/lib/__tests__/markov-spec.js
+++ src/lib/__tests__/markov-spec.js
@@ -2,11 +2,13 @@
 
 import Markov from '../markov';
 
-test('initializes text', async () => {
-  const foo: string = 'hai';
-});
+test('generates random text', () => {
+  const inputText = 'foo bar baw';
+  const markov = new Markov(inputText);
 
-test('generates new random text', async () => {
-  const foo: string = 'hai';
-  expect('3').toBe('3');
+  const nextState = 'bar baw';
+
+  const realText = markov.generate({ start: 'bar', input: inputText});
+
+  expect(realText).toEqual(nextState);
 });
--- src/lib/markov.js
+++ src/lib/markov.js
@@ -1,7 +1,18 @@
 // @flow
 
-export default function markov () {
-  /*
-    injest example test
-  */
+type generateOpts = {
+  input: string;
+  start: string;
+}
+
+export default class Markov {
+  sourceText: string;
+
+  constructor(inputText: string) {
+    this.sourceText = inputText;
+  }
+
+  generate = (opts: generateOpts) => {
+    console.log(this.sourceText);
+  }
 }
