96ddcfe39d06c5d1949375323e81f7544708abd1
songawee <songawee@gmail.com>
2016-12-28 16:29:21 -0700

    add base implementation

--- src/lib/__tests__/markov-spec.js
+++ src/lib/__tests__/markov-spec.js
@@ -2,13 +2,46 @@
 
 import Markov from '../markov';
 
-test('generates random text', () => {
-  const inputText = 'foo bar baw';
+test('builds data structure', () => {
+  const inputText = 'foo bar foo baw foo bat foo bat barf';
   const markov = new Markov(inputText);
 
-  const nextState = 'bar baw';
+  const realText = markov.buildIndex(inputText);
 
-  const realText = markov.generate({ start: 'bar', input: inputText});
+  const foo = realText.get('foo').get('connections');
+  expect(Object.keys(foo).length).toBe(3);
+  expect(foo.bar.count).toBe(1);
+  expect(foo.baw.count).toBe(1);
+  expect(foo.bat.count).toBe(2);
 
-  expect(realText).toEqual(nextState);
+  const baw = realText.get('baw').get('connections');
+  expect(Object.keys(baw).length).toBe(1);
+  expect(baw.foo.count).toBe(1);
+
+  const bar = realText.get('bar').get('connections');
+  expect(Object.keys(bar).length).toBe(1);
+  expect(bar.foo.count).toBe(1);
+
+  const bat = realText.get('bat').get('connections');
+  expect(Object.keys(bat).length).toBe(2);
+  expect(bat.foo.count).toBe(1);
+  expect(bat.barf.count).toBe(1);
+});
+
+test('builds random string from existing string', () => {
+  const inputText = 'the poop the poop but recognized a clear subsequent shift in power among levels of stratification due to the status and power differentials Analyzing the entire edit history of Wikipedia up to July 2006, the same study determined that the influence of administrator edits on contents has steadily diminished since 2003, when administrators performed roughly 50% of total edits, to 2006 when only 10% of the edits were performed by administrators. This happened despite the fact that the average number of edits per administrator had increased more than fivefold during the same period. This phenomenon was labeled the rise of the crowd by the authors of the paper. An analysis that used as metric the number of words edited instead of the number of edit actions showed a similar pattern. Because the admin class is somewhat arbitrary with respect to the number of edits, the study also considered a breakdown of users in categories based on the number of edits performed. The results for elite users, i.e. users with more than 10,000 edits, were somewhat in line with those obtained for administrators, except that the number of words changed by elite users has kept up with the changes made by novice users, even though the number of edits made by novice users has grown proportionally faster. The elite users were attributed about 30% of the changes for 2006. The study concludes';
+  const markov = new Markov(inputText);
+
+  const realText = markov.generate();
+  expect(realText.split(' ').filter((item) => {
+    return !inputText.includes(item);
+  }).length).toBe(0);
+});
+
+test('builds random string from single word', () => {
+  const inputText = 'foo';
+  const markov = new Markov(inputText);
+
+  const realText = markov.generate();
+  expect(realText).toBe(inputText);
 });
--- src/lib/markov.js
+++ src/lib/markov.js
@@ -6,13 +6,114 @@ type generateOpts = {
 }
 
 export default class Markov {
-  sourceText: string;
+  wordMap: Map<string>;
 
   constructor(inputText: string) {
-    this.sourceText = inputText;
+    this.wordMap = this.buildIndex(inputText);
   }
 
   generate = (opts: generateOpts) => {
-    console.log(this.sourceText);
+    let result = '';
+    const totalWords = this.wordMap.size;
+    const keys = this.wordMap.keys();
+    const words = [];
+
+    for (const word of keys) {
+      words.push(word);
+    }
+
+    const startingIndex = function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }(0, totalWords);
+
+    const startingWord = words[startingIndex];
+
+    let currentWord = startingWord;
+    result = result + currentWord;
+
+    while (result.length < 80) {
+      const currentWordConnections = this.wordMap.get(currentWord);
+      const connections = currentWordConnections.get('connections');
+
+      const total = Object.keys(connections).reduce((acc, val) => {
+        return acc + connections[val].count;
+      }, 0);
+
+      if (!total) {
+        break;
+      }
+
+      const list = Object.keys(connections);
+
+      const weights = list.map((listItem) => {
+        return connections[listItem].count / total;
+      });
+
+      const random_item = this.getRandomItem(list, weights);
+
+      if (!random_item) {
+        break;
+      }
+
+      currentWord = random_item;
+
+      result = `${result} ${currentWord}`;
+    }
+
+    return result;
+  }
+
+  getRandomItem(list, weight) {
+    const random_num = Math.random();
+    let weight_sum = 0;
+
+    for (let i in list) {
+      weight_sum += weight[i];
+
+      if (random_num <= weight_sum) {
+        return list[i];
+      }
+    }
+  };
+
+  buildIndex(text: string) {
+    const words = text.split(' ');
+    const wordMap = new Map();
+
+    words.forEach((word, idx) => {
+      const nextWord = words[idx + 1];
+
+      if (wordMap.has(word)) {
+        const wordConnections = wordMap.get(word).get('connections');
+
+        if (nextWord) {
+          let existingCount = wordConnections[nextWord] && wordConnections[nextWord].count;
+          if (existingCount) {
+            existingCount++;
+          }
+          wordConnections[nextWord] = {
+            count: existingCount || 1
+          };
+
+          wordMap.get(word).set('connections', wordConnections);
+        }
+      } else {
+        let connection = {};
+
+        if (nextWord) {
+          connection = {
+            [nextWord]: {
+              count: 1
+            }
+          }
+        }
+
+        wordMap.set(word, new Map([["connections", connection]]));
+      }
+    });
+
+    return wordMap;
   }
 }
